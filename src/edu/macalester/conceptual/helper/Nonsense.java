319b9f6d22ea3cb128112cc3fa8f781f1d289cd3
Paul Cantrell <cantrell@pobox.com>
2022-10-10 00:38:26 -0500

    Nonsense words!

---
+++ src/edu/macalester/conceptual/helper/Nonsense.java
@@ -0,0 +1,111 @@
+package edu.macalester.conceptual.helper;
+
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
+
+import static com.github.javaparser.utils.Utils.capitalize;
+
+public class Nonsense {
+    private static final WeightedChoices<String>
+        ONSETS = WeightedChoices.fromResource("syllable-parts/onsets"),
+        NUCLEI = WeightedChoices.fromResource("syllable-parts/nuclei"),
+        CODAS = WeightedChoices.fromResource("syllable-parts/codas");
+
+    public static VariableDeclarator variable(PuzzleContext ctx) {
+        return variable(ctx, type(ctx));
+    }
+
+    public static VariableDeclarator variable(PuzzleContext ctx, Type type) {
+        return new VariableDeclarator(type, new SimpleName(variableName(ctx)));
+    }
+
+    private static Type type(PuzzleContext ctx) {
+        return new ClassOrInterfaceType(null, typeName(ctx));
+    }
+
+    public static String typeName(PuzzleContext ctx) {
+        return words(ctx, NameFormat.CAPITALIZED_CAMEL_CASE, 2, 12, 5);
+    }
+
+    public static String methodName(PuzzleContext ctx) {
+        return words(ctx, NameFormat.LOWER_CAMEL_CASE, 3, 6, 4);
+    }
+
+    public static String propertyName(PuzzleContext ctx) {
+        return words(ctx, NameFormat.LOWER_CAMEL_CASE, 2, 5, 3);
+    }
+
+    public static String variableName(PuzzleContext ctx) {
+        return words(ctx, NameFormat.LOWER_CAMEL_CASE, 1, 4, 1);
+    }
+
+    public static String constantName(PuzzleContext ctx) {
+        return words(ctx, NameFormat.SCREAMING_SNAKE_CASE, 2, 10, 6);
+    }
+
+    public static String words(
+        PuzzleContext ctx,
+        NameFormat format,
+        int minWordLen,
+        int maxWordLen, int minTotalLen
+    ) {
+        StringBuilder result = new StringBuilder();
+        while (result.length() < minTotalLen) {
+            String nextWord = word(ctx);
+            if (nextWord.length() < minWordLen || nextWord.length() > maxWordLen) {
+                continue;
+            }
+            result.append(
+                result.isEmpty()
+                    ? format.formatFirstWord(nextWord)
+                    : format.formatSubsequentWord(nextWord));
+        }
+        return result.toString();
+    }
+
+    public static String word(PuzzleContext ctx) {
+        StringBuilder result = new StringBuilder();
+        for (int i = ctx.getRandom().nextInt(2); i >= 0; i--) {
+            result.append(syllable(ctx));
+        }
+        return result.toString();
+    }
+
+    public static String syllable(PuzzleContext ctx) {
+        return ONSETS.choose(ctx) + NUCLEI.choose(ctx) + CODAS.choose(ctx);
+    }
+
+    public static enum NameFormat {
+        LOWER_CAMEL_CASE {
+            String formatFirstWord(String word) {
+                return word;
+            }
+            String formatSubsequentWord(String word) {
+                return capitalize(word);
+            }
+        },
+
+        CAPITALIZED_CAMEL_CASE {
+            String formatFirstWord(String word) {
+                return capitalize(word);
+            }
+            String formatSubsequentWord(String word) {
+                return capitalize(word);
+            }
+        },
+
+        SCREAMING_SNAKE_CASE {
+            String formatFirstWord(String word) {
+                return word.toUpperCase();
+            }
+            String formatSubsequentWord(String word) {
+                return "_" + word.toUpperCase();
+            }
+        };
+
+        abstract String formatFirstWord(String word);
+        abstract String formatSubsequentWord(String word);
+    }
+}
---
+++ src/edu/macalester/conceptual/helper/WeightedChoices.java
@@ -0,0 +1,76 @@
+package edu.macalester.conceptual.helper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class WeightedChoices<Choice> {
+    private final List<WeightedChoice> choices;
+    private long totalWeight = 0;
+
+    public static WeightedChoices<String> fromResource(String resPath) {
+        var result = new WeightedChoices<String>();
+        var reader = new BufferedReader(
+            new InputStreamReader(
+                Objects.requireNonNull(
+                    WeightedChoices.class.getClassLoader().getResourceAsStream(resPath))));
+        try (reader) {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                if (line.isBlank()) {
+                    continue;
+                }
+                var fields = line.split("\\s+");
+                if (fields.length != 2) {
+                    throw new IOException("Illegal line format: " + line);
+                }
+                result.add(fields[0], Long.parseLong(fields[1]));
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("Unable to load choices from " + resPath, e);
+        }
+        return result;
+    }
+
+    public WeightedChoices() {
+        choices = new ArrayList<>();
+    }
+
+    public void add(Choice choice, long weight) {
+        if (weight < 0) {
+            throw new IllegalArgumentException("weight " + weight + " cannot be < 0");
+        }
+        choices.add(new WeightedChoice(choice, weight));
+        totalWeight += weight;
+    }
+
+    public Choice choose(PuzzleContext context) {
+        if (totalWeight <= 0) {
+            throw new IllegalStateException("WeightedChoices has no choices with nonzero weight");
+        }
+        long w = context.getRandom().nextLong(totalWeight);
+        for (var cur : choices) {
+            w -= cur.weight;
+            if (w <= 0) {
+                return cur.choice;
+            }
+        }
+        throw new AssertionError("Ran out of choices with " + w + " weight left");
+    }
+
+    private class WeightedChoice {
+        private final Choice choice;
+        private final long weight;
+
+        public WeightedChoice(Choice choice, long weight) {
+            this.choice = choice;
+            this.weight = weight;
+        }
+    }
+}
