4969d409a694340861a494d15f924173691f0f05
Paul Cantrell <cantrell@pobox.com>
2022-10-13 23:52:10 -0500

    Added parens and negation to AstUtils

--- src/edu/macalester/conceptual/ast/AstUtils.java
+++ src/edu/macalester/conceptual/ast/AstUtils.java
@@ -3,14 +3,27 @@ package edu.macalester.conceptual.ast;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.CastExpr;
+import com.github.javaparser.ast.expr.ConditionalExpr;
+import com.github.javaparser.ast.expr.EnclosedExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.InstanceOfExpr;
 import com.github.javaparser.ast.expr.IntegerLiteralExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.expr.UnaryExpr;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
+import static com.github.javaparser.ast.expr.BinaryExpr.Operator.*;
+import static com.github.javaparser.ast.expr.UnaryExpr.Operator.*;
 
 public enum AstUtils {
     ; // no cases; static methods only
@@ -37,5 +50,96 @@ public enum AstUtils {
         return Arrays.stream(nodes)
             .map(Node::toString)
             .collect(Collectors.joining("\n"));
+    }
+
+    /**
+     * Add parentheses as necessary within the given AST to ensure that the emitted code respects
+     * the actual structure of the tree.
+     *
+     * (Somewhat surprising JavaParser doesn't already provide this!)
+     */
+    public static <NodeType extends Node> NodeType withParensAsNeeded(NodeType expr) {
+        @SuppressWarnings("unchecked")
+        NodeType copy = (NodeType) expr.clone();
+        insertParensAsNeeded(copy);
+        return copy;
+    }
+
+    private static void insertParensAsNeeded(Node node) {
+        new ArrayList<>(node.getChildNodes())
+            .forEach(AstUtils::insertParensAsNeeded);
+        if (node instanceof Expression expr
+            && node.getParentNode().orElse(null) instanceof Expression parentExpr
+            && getPrecedence(parentExpr) > getPrecedence(expr)
+        ) {
+            parentExpr.replace(expr, new EnclosedExpr(expr));
+        }
+    }
+
+    private static int getPrecedence(Expression expr) {
+        // Precedences taken from https://introcs.cs.princeton.edu/java/11precedence/
+        if (expr instanceof UnaryExpr unary) {
+            return switch(unary.getOperator()) {
+                case POSTFIX_INCREMENT, POSTFIX_DECREMENT -> 15;
+                default -> 14;
+            };
+        } else if (expr instanceof CastExpr || expr instanceof ObjectCreationExpr) {
+            return 13;
+        } else if (expr instanceof BinaryExpr binary) {
+            return switch(binary.getOperator()) {
+                case MULTIPLY, DIVIDE, REMAINDER -> 12;
+                case PLUS, MINUS -> 11;
+                case LEFT_SHIFT, SIGNED_RIGHT_SHIFT, UNSIGNED_RIGHT_SHIFT -> 10;
+                case LESS, GREATER, LESS_EQUALS, GREATER_EQUALS -> 9;
+                case EQUALS, NOT_EQUALS -> 8;
+                case BINARY_AND -> 7;
+                case XOR -> 6;
+                case BINARY_OR -> 5;
+                case AND -> 4;
+                case OR -> 3;
+            };
+        } else if (expr instanceof InstanceOfExpr) {
+            return 9;
+        } else if (expr instanceof AssignExpr) {
+            return 1;
+        } else if (expr instanceof ConditionalExpr) {
+            return 2;
+        } else {
+            return 17;
+        }
+    }
+
+    public static Expression negated(Expression node) {
+        if (node instanceof UnaryExpr expr) {
+            if (expr.getOperator() == LOGICAL_COMPLEMENT) {
+                return expr.getExpression();
+            } else {
+                return expr;
+            }
+        } else if (node instanceof BinaryExpr expr) {
+            return switch(expr.getOperator()) {
+                case AND -> deMorgan(expr, OR);
+                case OR -> deMorgan(expr, AND);
+
+                case EQUALS -> replaceOperator(expr, NOT_EQUALS);
+                case NOT_EQUALS -> replaceOperator(expr, EQUALS);
+                case GREATER -> replaceOperator(expr, LESS_EQUALS);
+                case GREATER_EQUALS -> replaceOperator(expr, LESS);
+                case LESS -> replaceOperator(expr, GREATER_EQUALS);
+                case LESS_EQUALS -> replaceOperator(expr, GREATER);
+
+                default -> new UnaryExpr(new EnclosedExpr(expr), LOGICAL_COMPLEMENT);
+            };
+        } else {
+            return new UnaryExpr(node, LOGICAL_COMPLEMENT);
+        }
+    }
+
+    private static Expression deMorgan(BinaryExpr expr, BinaryExpr.Operator operator) {
+        return new BinaryExpr(negated(expr.getLeft()), negated(expr.getRight()), operator);
+    }
+
+    private static Expression replaceOperator(BinaryExpr expr, BinaryExpr.Operator operator) {
+        return new BinaryExpr(expr.getLeft(), expr.getRight(), operator);
     }
 }
