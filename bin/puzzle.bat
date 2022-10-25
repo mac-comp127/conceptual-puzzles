b0d4d9bd75d62f06226a0ee45ac9c633b2e8207e
Paul Cantrell <cantrell@pobox.com>
2022-10-24 20:44:09 -0500

    Fixed sequential Gradle calls in .bat

--- bin/puzzle.bat
+++ bin/puzzle.bat
@@ -10,7 +10,7 @@ chcp 65001 >nul
 REM Build with Gradle in regular console mode, so that we get a progress
 REM bar for the slow first-time build
 
-"%~dp0\..\gradlew.bat" build --project-dir "%~dp0\.." --warn
+call "%~dp0\..\gradlew.bat" build --project-dir "%~dp0\.." --warn
 
 REM Now run the app with --console=plain, because Gradle likes to
 REM clobber the program output with its own progress bar.
