5584e1ecf97ddea1b9b6790035ff38eab9ca6d4c
Paul Cantrell <cantrell@pobox.com>
2022-10-24 20:09:48 -0500

    Gradle build w/progress bar, then run without

--- bin/puzzle.bat
+++ bin/puzzle.bat
@@ -7,6 +7,21 @@ set "COLUMNS=%CONSOLE_WIDTH%"
 REM Switch to UTF-8
 chcp 65001 >nul
 
+REM Build with Gradle in regular console mode, so that we get a progress
+REM bar for the slow first-time build
+
+"%~dp0\..\gradlew.bat" build --project-dir "%~dp0\.." --warn
+
+REM Now run the app with --console=plain, because Gradle likes to
+REM clobber the program output with its own progress bar.
+REM
+REM Note the space before $* below:
+REM
+REM Gradle blows up if --args receives an empty string as its argument.
+REM However, it trims space from the end of the --args argument, and
+REM a single space thus shows up as an empty args array by the time
+REM it makes it to our main method.
+
 "%~dp0\..\gradlew.bat" ^
     run-cli ^
     --project-dir "%~dp0\.." ^
