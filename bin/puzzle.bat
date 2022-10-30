03803c69df7899135dc1734f00a19068307766d4
Paul Cantrell <cantrell@pobox.com>
2022-10-30 13:18:49 -0500

    Assume bin\puzzle on Windows

--- bin/puzzle.bat
+++ bin/puzzle.bat
@@ -13,6 +13,13 @@ REM bar for the slow first-time build
 
 call "%~dp0\..\gradlew.bat" compileJava --project-dir "%~dp0\.." --warn
 
+REM It's apparently not possible to get bat file's path as the user typed it:
+REM https://superuser.com/questions/1748050/is-it-possible-for-a-windows-batch-file-to-get-its-own-name-as-the-user-typed-i
+REM We thus assume the student is following the directions from Moodle, is in
+REM the project root dir, and typed `bin\puzzle` to run this script:
+
+set "puzzle_command=bin\puzzle"
+
 REM Now run the app with --console=plain, because Gradle likes to
 REM clobber the program output with its own progress bar.
 REM
