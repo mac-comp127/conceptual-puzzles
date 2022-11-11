3b1b2a2867b46e04748336db2cd895c2be0c8dc9
Paul Cantrell <cantrell@pobox.com>
2022-11-11 15:08:06 -0600

    Use a better dictionary for prohibited words
    The previous one didn't actually include profanities!

--- src/edu/macalester/conceptual/util/ExcludedWords.java
+++ src/edu/macalester/conceptual/util/ExcludedWords.java
@@ -1,13 +1,16 @@
 package edu.macalester.conceptual.util;
 
-import com.google.common.collect.Streams;
 import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnels;
 
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -42,29 +45,30 @@ class ExcludedWords {
      * Regenerates the serialized prohibited words file from the macOS dict file + all Java keywords.
      */
     public static void main(String[] args) throws IOException {
-        long wordCount = allDictWords().count();
-        System.out.println(wordCount + " prohibited words");
-        BloomFilter<String> bloom = BloomFilter.create(Funnels.unencodedCharsFunnel(), wordCount, 0.01);
-        allDictWords().forEach(bloom::put);
+        var words = allDictWords();
+        System.out.println(words.size() + " prohibited words");
+        BloomFilter<String> bloom = BloomFilter.create(Funnels.unencodedCharsFunnel(), words.size(), 0.01);
+        words.forEach(bloom::put);
         bloom.writeTo(new FileOutputStream("res/" + BLOOM_FILE_NAME));
         System.out.println("New prohibited words file created");
     }
 
     @SuppressWarnings("resource")
-    private static Stream<String> allDictWords() throws IOException {
-        return Streams.concat(
-            // Java reserved words
-            Stream.of(
-                "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
-                "continue", "default", "double", "do", "else", "enum", "extends", "false", "final",
-                "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int",
-                "interface", "long", "native", "new", "null", "package", "private", "protected", "public",
-                "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw",
-                "throws", "transient", "true", "try", "void", "volatile", "while"),
+    private static List<String> allDictWords() throws IOException {
+        var dicts = Stream.of(
+            Path.of("res/java-reserved-words.txt").toUri().toURL(),
+            new URL("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"));
 
-            // All words of reasonable length in dictionary
-            Files.lines(Path.of("/usr/share/dict/words"))  // macOS dictionary file
-                .filter(word -> word.length() >= 3 && word.length() <= 8));
+        return dicts.flatMap(url -> {
+            try {
+                System.out.println("Reading words from " + url);
+                return new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))
+                    .lines()
+                    .filter(word -> word.length() >= 3 && word.length() <= 8);
+            } catch(IOException e) {
+                throw new RuntimeException("Unable to read words from " + url, e);
+            }
+        }).toList();  // Bloom needs a count _before_ the traversal, so just slurp them all into memory
     }
 
     public boolean contains(String word) {
