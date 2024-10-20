a21d1d424d4c3591eadd9f2f6d84e853e5579b15
Paul Cantrell <cantrell@pobox.com>
2024-10-20 16:04:13 -0500

    Added contextual instructions to closure puzzle

--- src/edu/macalester/conceptual/puzzles/closures/ClosuresPuzzle.java
+++ src/edu/macalester/conceptual/puzzles/closures/ClosuresPuzzle.java
@@ -16,7 +16,7 @@ public class ClosuresPuzzle implements Puzzle {
 
     @Override
     public String description() {
-        return "Closure execution order";
+        return "Closures and event handling";
     }
 
     @Override
@@ -31,6 +31,37 @@ public class ClosuresPuzzle implements Puzzle {
 
     @Override
     public void generate(PuzzleContext ctx) {
-        new ClosureTracingPuzzle(ctx).generate();
+        ctx.output().paragraph(
+            """
+            Suppose we are in an environment where `CLICK`, `TICK`, and `KEY` events can occur, and
+            suppose we have the following functions available:
+            """
+        );
+
+        ctx.output().paragraph("`twice(closure)`");
+        ctx.output().blockquote("Immediately executes `closure` two times.");
+
+        ctx.output().paragraph("`onClick(closure)`");
+        ctx.output().blockquote("Executes `closure` after every subsequent `CLICK` event.");
+
+        ctx.output().paragraph("`onKeyPress(closure)`");
+        ctx.output().blockquote("Executes `closure` after every subsequent `KEY` event.");
+
+        ctx.output().paragraph("`afterDelay(tickCount, closure)`");
+        ctx.output().blockquote(
+            "Executes `closure` once after exactly `tickCount` `TICK` events have occurred."
+        );
+
+        ctx.output().paragraph(
+            """
+            Multiple closures can be registered to handle the same event. If this happens, they
+            execute in the order they were registered.
+            """
+        );
+        ctx.output().dividerLine(false);
+
+        ctx.section(() -> {
+            new ClosureTracingPuzzle(ctx).generate();
+        });
     }
 }
