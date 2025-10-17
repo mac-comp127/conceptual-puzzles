8d0f571deb94ec9b3501c2096590f06f505dc8e4
Dan Drake <ddrake1@macalester.edu>
2025-10-17 16:01:06 -0500

    add random names; configurable depth of chain

--- src/edu/macalester/conceptual/puzzles/constructorchains/ConstructorChainPuzzle.java
+++ src/edu/macalester/conceptual/puzzles/constructorchains/ConstructorChainPuzzle.java
@@ -7,12 +7,17 @@ import edu.macalester.conceptual.util.CodeFormatting;
 import edu.macalester.conceptual.util.Evaluator;
 import edu.macalester.conceptual.util.VariablePool;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
@@ -35,7 +40,11 @@ public class ConstructorChainPuzzle implements Puzzle {
 
     @Override
     public void generate(PuzzleContext ctx) {
-        ctx.output().paragraph("What gets printed when you create an instance of [someclass]?");
+
+        ctx.output().paragraph("With the following class declarations:");
+
+        Random rand = ctx.getRandom();
+        int depth = rand.nextInt(0, 5);
 
 
         // cat file-with-class-decl | bin/astprinter class
@@ -43,25 +52,43 @@ public class ConstructorChainPuzzle implements Puzzle {
         // the AST is built bottom-up, and the text output uses indentation for the tree,
         // so "bottom-up" here means "rightmost-indented left" there, so to speak
 
-        var msg = new StringLiteralExpr("Organism default constructor");
-        var printit = new MethodCallExpr("printit", msg);
-        var printblock = new BlockStmt().addStatement(printit);
-
-
-        var decl = AstUtils.publicClassDecl("Organism");
+        String className = RandomLoch.getTypeName(ctx);
+        var decl = AstUtils.publicClassDecl(className);
         decl.addConstructor(Modifier.Keyword.PUBLIC);
+        maybeAddPrintLn(decl, className, rand);
 
-        var ctor = decl.getDefaultConstructor().get().setBody(printblock);
+        ctx.output().codeBlock(CodeFormatting.prettify(decl));
 
+        List<ClassOrInterfaceDeclaration> classes = new ArrayList<ClassOrInterfaceDeclaration>();
 
-        ctx.output().codeBlock(
-            CodeFormatting.prettify(decl));
+        classes.add(decl);
 
-        var decl2 = AstUtils.publicClassDecl("Animal");
-        String organismClass = decl.getNameAsString();
-        decl2.addExtendedType(organismClass);
+        for (int i = 0; i < depth; i++) {
 
-        ctx.output().codeBlock(
-            CodeFormatting.prettify(decl2));
+            // higher difficulties: randomly create an instance of some class earlier up the hierarchy in
+            // constructor here
+            var nameExpr = new NameExpr("Racadal");
+            // var objcreation = new ObjectCreationExpr(classes.get(0).getClass().getTty);
+
+            String parentClass = decl.getNameAsString();
+            className = RandomWeatherPlace.getTypeName(ctx);
+            decl = AstUtils.publicClassDecl(className);
+            decl.addExtendedType(parentClass);
+            decl.addConstructor(Modifier.Keyword.PUBLIC);
+            maybeAddPrintLn(decl, className, rand);
+            ctx.output().codeBlock(CodeFormatting.prettify(decl));
+            classes.add(decl);
+        }
+
+        ctx.output().paragraph("What gets printed when you create an instance of " + className + "?");
+    }
+
+    private void maybeAddPrintLn(ClassOrInterfaceDeclaration decl, String className, Random rand) {
+        if (rand.nextBoolean()) {
+            var msg = new StringLiteralExpr(className + " default constructor");
+            var sout = new MethodCallExpr("System.out.println", msg);
+            var printblock = new BlockStmt().addStatement(sout);
+            decl.getDefaultConstructor().get().setBody(printblock);
+        }
     }
 }
