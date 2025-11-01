cb4a69a00fd986c083a669f3cada5bb7bd319c24
Dan Drake <ddrake1@macalester.edu>
2025-11-01 12:59:24 -0500

    non-default constructor, creation of ancestor's non-default ctor

--- src/edu/macalester/conceptual/puzzles/constructorchains/ConstructorChainPuzzle.java
+++ src/edu/macalester/conceptual/puzzles/constructorchains/ConstructorChainPuzzle.java
@@ -1,32 +1,21 @@
 package edu.macalester.conceptual.puzzles.constructorchains;
 
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import edu.macalester.conceptual.Puzzle;
 import edu.macalester.conceptual.context.PuzzleContext;
 import edu.macalester.conceptual.util.AstUtils;
 import edu.macalester.conceptual.util.ChoiceDeck;
 import edu.macalester.conceptual.util.CodeFormatting;
-import edu.macalester.conceptual.util.Evaluator;
-import edu.macalester.conceptual.util.VariablePool;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-
-import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.AssignExpr;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.expr.ObjectCreationExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.symbolsolver.javaparsermodel.contexts.VariableDeclarationExprContext;
-import com.github.javaparser.ast.expr.StringLiteralExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 
 
 public class ConstructorChainPuzzle implements Puzzle {
@@ -51,7 +40,7 @@ public class ConstructorChainPuzzle implements Puzzle {
         ctx.output().paragraph("With the following class declarations:");
 
         Random rand = ctx.getRandom();
-        int depth = rand.nextInt(0, 5);
+        int depth = rand.nextInt(0, 8);
 
         // cat file-with-class-decl | bin/astprinter class
         // use the output to inform the code below that builds your AST;
@@ -61,11 +50,12 @@ public class ConstructorChainPuzzle implements Puzzle {
         String className = RandomLoch.getTypeName(ctx);
         var decl = AstUtils.publicClassDecl(className);
         decl.addConstructor(Modifier.Keyword.PUBLIC);
-        maybeAddPrintLn(decl, className, rand);
+        maybeAddPrintLn(decl, rand);
+        maybeAddNonDefaultCtor(decl, rand);
 
         ctx.output().codeBlock(CodeFormatting.prettify(decl));
 
-        List<ClassOrInterfaceDeclaration> classes = new ArrayList<ClassOrInterfaceDeclaration>();
+        List<ClassOrInterfaceDeclaration> classes = new ArrayList<>();
 
         classes.add(decl);
 
@@ -74,16 +64,19 @@ public class ConstructorChainPuzzle implements Puzzle {
             className = RandomWeatherPlace.getTypeName(ctx);
             decl = AstUtils.publicClassDecl(className);
             decl.addExtendedType(parentClass);
-            decl.addConstructor(Modifier.Keyword.PUBLIC);
+            decl.addConstructor();
+            maybeAddNonDefaultCtor(decl, rand);
 
             // TODO: for extra difficulty, shuffle the order of these
             maybeAddObjCreation(decl, classes, ctx);
-            maybeAddPrintLn(decl, className, rand);
+            maybeAddNonDefaultCtorObjectCreation(decl, classes, ctx);
+            maybeAddPrintLn(decl, rand);
 
             ctx.output().codeBlock(CodeFormatting.prettify(decl));
             classes.add(decl);
         }
 
+        // FIXME need to specify which constructor, if leaf class has more than one
         ctx.output().paragraph("What gets printed when you create an instance of " + className + "?");
     }
 
@@ -91,29 +84,56 @@ public class ConstructorChainPuzzle implements Puzzle {
     private ExpressionStmt getObjectCreationStmt(String staticTypeName, String variableName, String runtimeTypeName) {
 
         VariableDeclarator varDecl = new VariableDeclarator(
-            new ClassOrInterfaceType(null, staticTypeName),
-            variableName,
-            new ObjectCreationExpr(null, new ClassOrInterfaceType(null, runtimeTypeName),
-                new com.github.javaparser.ast.NodeList<>()));
+                new ClassOrInterfaceType(null, staticTypeName),
+                variableName,
+                new ObjectCreationExpr(null, new ClassOrInterfaceType(null, runtimeTypeName),
+                        new com.github.javaparser.ast.NodeList<>()));
         VariableDeclarationExpr varDeclExpr = new VariableDeclarationExpr(varDecl);
         return new ExpressionStmt(varDeclExpr);
     }
 
-    private void maybeAddPrintLn(ClassOrInterfaceDeclaration decl, String className, Random rand) {
+    private ExpressionStmt getObjectCreationStmtWithParam(String staticTypeName, String variableName, String runtimeTypeName, String param) {
+
+        NodeList<Expression> nodeList = new NodeList<>(new IntegerLiteralExpr(param));
+        VariableDeclarator varDecl = new VariableDeclarator(
+                new ClassOrInterfaceType(null, staticTypeName),
+                variableName,
+                new ObjectCreationExpr(null, new ClassOrInterfaceType(null, runtimeTypeName), nodeList));
+        VariableDeclarationExpr varDeclExpr = new VariableDeclarationExpr(varDecl);
+        return new ExpressionStmt(varDeclExpr);
+    }
+
+
+    private void maybeAddPrintLn(ClassOrInterfaceDeclaration decl, Random rand) {
         if (rand.nextBoolean()) {
-            var msg = new StringLiteralExpr(className + " default constructor");
+            var msg = new StringLiteralExpr(decl.getName() + " default constructor");
             var sout = new MethodCallExpr("System.out.println", msg);
             decl.getDefaultConstructor().get().getBody().addStatement(sout);
         }
     }
 
+    private void maybeAddNonDefaultCtor(ClassOrInterfaceDeclaration declaration, Random rand) {
+        if (rand.nextBoolean()) {
+            var nonDefaultCtor = declaration.addConstructor(Modifier.Keyword.PUBLIC);
+            nonDefaultCtor.addParameter("int", "n");
+            // TODO: do the "maybe" adds for object creation, nondefault ctor object
+
+            if (rand.nextBoolean()) {
+                var msg = new StringLiteralExpr(declaration.getName() + " constructor, n = \" + n + \".");
+                var sout = new MethodCallExpr("System.out.println", msg);
+                nonDefaultCtor.getBody().addStatement(sout);
+            }
+        }
+    }
+
+
     private void maybeAddObjCreation(
-        ClassOrInterfaceDeclaration decl,
-        List<ClassOrInterfaceDeclaration> classes,
-        PuzzleContext ctx) {
+            ClassOrInterfaceDeclaration decl,
+            List<ClassOrInterfaceDeclaration> classes,
+            PuzzleContext ctx) {
 
         Random rand = ctx.getRandom();
-        if (true /* rand.nextBoolean() */) {
+        if (rand.nextBoolean()) {
 
             var superclassesDeck = new ChoiceDeck<>(ctx, classes);
             // TODO: for extra difficulty, allow static and runtime types to differ, so that the runtime type is
@@ -121,6 +141,30 @@ public class ConstructorChainPuzzle implements Puzzle {
             var superClassName = superclassesDeck.draw().getName().toString();
             var objCreationStmt = getObjectCreationStmt(superClassName, "my" + superClassName, superClassName);
             decl.getDefaultConstructor().get().getBody().addStatement(objCreationStmt);
+        }
+    }
+
+    private void maybeAddNonDefaultCtorObjectCreation(ClassOrInterfaceDeclaration decl,
+                                                      List<ClassOrInterfaceDeclaration> classes,
+                                                      PuzzleContext ctx) {
+        Random rand = ctx.getRandom();
+        if (true /* rand.nextBoolean() */) {
+
+            var superclassesDeck = new ChoiceDeck<>(ctx, classes);
+            var superClass = superclassesDeck.draw();
+
+            if (superClass.getConstructorByParameterTypes("int").isPresent()) {
+                // TODO: for extra difficulty, allow static and runtime types to differ, so that the runtime type is
+                // a superclass of the superclass!
+                var superClassName = superClass.getName().toString();
+                String param = String.valueOf(rand.nextInt(10, 20));
+                String variableName = "my" + superClassName + param;
+                var objCreationStmt = getObjectCreationStmtWithParam(superClassName, variableName, superClassName, param);
+                var declCtor = new ChoiceDeck<>(ctx, decl.getConstructors()).draw();
+                declCtor.getBody().addStatement(objCreationStmt);
+            }
+
+
         }
     }
 }
